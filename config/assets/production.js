7e18d026e6e8a688281e9f10e8a31541a656f655
neilhanekom <neilhanekom1@gmail.com>
2015-05-20 06:59:48 +0200

    set for Production

--- config/assets/production.js
+++ config/assets/production.js
@@ -1,23 +1,26 @@
 'use strict';
 
 module.exports = {
-	client: {
-		lib: {
-			css: [
+	clientLibCSS: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-			],
-			js: [
+				'public/lib/angular-material/angular-material.css'
+				],
+	clientLibJS: [
 				'public/lib/angular/angular.min.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-				'public/lib/angular-file-upload/angular-file-upload.min.js'
-			]
-		},
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/application.min.js'
-	}
+				'public/lib/angular-file-upload/angular-file-upload.min.js',
+				'public/lib/angular-material-icons/angular-material-icons.min.js',
+				'public/lib/angular-moment/angular-moment.min.js',
+				'public/lib/ng-file-upload/angular-file-upload.js',
+				'public/lib/ng-file-upload-shim/angular-file-upload-shim.js',
+				'public/lib/angular-messages/angular-messages.min.js',
+				'public/lib/jquery/dist/jquery.min.js'
+		],
+	css: 'public/dist/application.min.css',
+	js: 'public/dist/application.min.js'
 };
